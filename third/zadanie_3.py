70c3fe73e83e2ca2208ede3eeb44d643e506339a
n0fr0st <krasnosheckov.v@yandex.ru>
2023-03-12 10:59:18 +0300

    add the thir task

---
+++ third/zadanie_3.py
@@ -0,0 +1,12 @@
+biletNum = input()
+summRight = 0
+summLeft = 0
+for i in range(int(len(biletNum)/2)):
+    summLeft += int(biletNum[i])
+    summRight += int(biletNum[(len(biletNum)-1) - i])
+if len(biletNum)%2 != 0:
+    print(f"Вы ввели некорректное число - {biletNum}.")
+elif summLeft == summRight:
+    print(f"Поздравляем ваш билет - СЧАСТЛИВЫЙ!!!(сумма правой и левой части - {summLeft})")
+else:
+    print(f"Сумма левой части билета - {summLeft}, а правой - {summRight}.")
